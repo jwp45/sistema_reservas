d169e729a43d96060cbaa45be63d445a84efdeea
Your Name <you@example.com>
2026-05-22 20:56:46 -0300

    refactor: actualizar lógica de conexión y métodos en Database

--- controllers/database.py
+++ controllers/database.py
@@ -5,7 +5,7 @@ class Database:
         self.host = "localhost"
         self.user = "root"
         self.password = ""
-        self.database = "reservas_profesionales"
+        self.database = "clientes"
 
     def connect(self):
         try:
@@ -15,39 +15,33 @@ class Database:
                 password=self.password,
                 database=self.database
             )
-            return True
-        except mysql.connector.Error as err:
-            print(f"Error al conectar a la base de datos: {err}")
+            if self.connection.is_connected():
+                print("Conexión exitosa a la base de datos")
+                return True
+        except Exception as e:
+            print(f"Error al conectar a la base de datos: {e}")
             return False
 
-    def get_next_id(self):
-        if self.connect():
-            cursor = self.connection.cursor()
-            query = "SELECT MAX(id_clientes) FROM clientes"
-            cursor.execute(query)
-            result = cursor.fetchone()
-            cursor.close()
-            self.connection.close()
-            if result[0] is None:
-                return 1
-            else:
-                return result[0] + 1
-
     def insert_client(self, client_data):
-        if self.connect():
+        try:
             cursor = self.connection.cursor()
             query = "INSERT INTO clientes (nombre, apellido, email, telefono) VALUES (%s, %s, %s, %s)"
             cursor.execute(query, client_data)
             self.connection.commit()
-            cursor.close()
-            self.connection.close()
+            print("Cliente guardado exitosamente")
+        except Exception as e:
+            print(f"Error al guardar el cliente: {e}")
 
-    def get_all_clients(self):
-        if self.connect():
+    def get_next_id(self):
+        try:
             cursor = self.connection.cursor()
-            query = "SELECT id_clientes, nombre, apellido, email, telefono FROM clientes"
+            query = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'clientes' AND TABLE_NAME = 'clientes'"
             cursor.execute(query)
-            clients = cursor.fetchall()
-            cursor.close()
-            self.connection.close()
-            return clients
+            result = cursor.fetchone()
+            if result:
+                return result[0]
+            else:
+                return None
+        except Exception as e:
+            print(f"Error al obtener el próximo ID: {e}")
+            return None
